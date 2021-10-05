d657b22d2649b2c01344ff344ad85bf53f00fb60
ln8-8 <lyut.nersisyan@gmail.com>
2021-10-05 12:05:34 +0400

    Completely changed the NameOfFiles.sh to print all file names with subfolders

--- NameOfFiles.sh
+++ NameOfFiles.sh
@@ -2,4 +2,21 @@
 #Print all file names on given folder with subfolders
 #Lyudovikos 4/10/2021
 
-ls $1
+printNames () {
+	for i in $1/*
+	do
+		if [ -d $i ]
+		then 
+			echo "$i"
+			printNames $i
+		else
+			echo "$i"
+		fi
+	done
+}
+if [ -d $1 ]
+then 
+	printNames $1
+else
+	echo "The directory does not exist"
+fi
