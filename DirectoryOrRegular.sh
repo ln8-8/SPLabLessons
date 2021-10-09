b3a526177d32d054aec4377dd93441a4ea7a00c9
ln8-8 <lyut.nersisyan@gmail.com>
2021-10-09 19:13:49 +0400

    Added new variable for testing and new flag if we don't want to type

--- DirectoryOrRegular.sh
+++ DirectoryOrRegular.sh
@@ -2,12 +2,26 @@
 #This script report us if given file is regular file or directory
 #Lyudovikos 4/10/2021
 
+a=0;
+
 if [ -d $1 ]
-then 
+then
+        a=1
+	if [ "$2" != "-t" ]
+	then	
 	echo "$1 is a directory"
+	fi
 elif [ -f $1 ]
 then
+	a=2
+	if [ "$2" != "-t" ]
+	then
 	echo "$1 is a regular file"
+	fi
 else
+	a=3
+	if [ "$2" != "-t" ]
+	then
 	echo "$1 does not exist"
+	fi
 fi
