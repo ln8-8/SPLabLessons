baaaf9f124bfa73748e699e330d681d89d0301fc
ln8-8 <lyut.nersisyan@gmail.com>
2021-10-20 19:43:21 +0400

    Changed the name of variable re

--- ReverseNumber.sh
+++ ReverseNumber.sh
@@ -3,32 +3,32 @@
 
 givenNumber=$1
 reversedNumber=0
-re='^-?[0-9]+$'
+CheckGivenArgument='^-?[0-9]+$'
 
-if ! [[ $givenNumber =~ $re ]]
+if ! [[ $givenNumber =~ $CheckGivenArgument ]]
 then
     outputMessage="Sorry integers only"
     echo $outputMessage
 fi
 
-if [[ $1 -lt 0 &&  $givenNumber =~ $re ]]
+if [[ $1 -lt 0 &&  $givenNumber =~ $CheckGivenArgument ]]
 then
 	givenNumber=$(( $givenNumber * -1 ))
 fi
 
-while [[ $givenNumber -gt 0 && $givenNumber =~ $re  ]]
+while [[ $givenNumber -gt 0 && $givenNumber =~ $CheckGivenArgument ]]
 do 
 	tmp=$(( $givenNumber % 10  ))
 	reversedNumber=$(( $reversedNumber * 10 + $tmp ))
 	givenNumber=$(( $givenNumber / 10 ))
 done
 
-if [[ $1 -lt 0 && $givenNumber =~ $re ]]
+if [[ $1 -lt 0 && $givenNumber =~ $CheckGivenArgument ]]
 then
 	reversedNumber=$(( $reversedNumber * -1 ))
 fi
 
-if [[ "$2" != "-t" && $givenNumber =~ $re ]]
+if [[ "$2" != "-t" && $givenNumber =~ $CheckGivenArgument ]]
 then
 echo "$reversedNumber"
 fi
