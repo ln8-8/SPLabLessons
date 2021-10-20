235c67d3527de336b00b11508f4799b526840e19
ln8-8 <lyut.nersisyan@gmail.com>
2021-10-20 22:39:45 +0400

    Changed the ReverseNumber.sh to reversebig numbers

--- ReverseNumber.sh
+++ ReverseNumber.sh
@@ -3,32 +3,13 @@
 
 givenNumber=$1
 reversedNumber=0
-CheckGivenArgument='^-?[0-9]+$'
+CheckGivenArgument='^[0-9]+$'
 
 if ! [[ $givenNumber =~ $CheckGivenArgument ]]
 then
-    outputMessage="Sorry integers only"
-    echo $outputMessage
-fi
-
-if [[ $1 -lt 0 &&  $givenNumber =~ $CheckGivenArgument ]]
-then
-	givenNumber=$(( $givenNumber * -1 ))
-fi
-
-while [[ $givenNumber -gt 0 && $givenNumber =~ $CheckGivenArgument ]]
-do 
-	tmp=$(( $givenNumber % 10  ))
-	reversedNumber=$(( $reversedNumber * 10 + $tmp ))
-	givenNumber=$(( $givenNumber / 10 ))
-done
-
-if [[ $1 -lt 0 && $givenNumber =~ $CheckGivenArgument ]]
-then
-	reversedNumber=$(( $reversedNumber * -1 ))
-fi
-
-if [[ "$2" != "-t" && $givenNumber =~ $CheckGivenArgument ]]
-then
-echo "$reversedNumber"
+    	outputMessage="Given argument is not an unsigned integer"
+    	echo $outputMessage
+else
+	reversedNumber=$(echo $givenNumber | rev )
+	echo $reversedNumber
 fi
--- Tests/TestForReverse.sh
+++ Tests/TestForReverse.sh
@@ -2,7 +2,7 @@
 
 touch fileForOutputText.txt
 source ../ReverseNumber.sh abc > fileForOutputText.txt
-if [ "$outputMessage" != "Sorry integers only" ]
+if [ "$outputMessage" != "Given argument is not an unsigned integer" ]
 then 
 	echo "failed when the given argument isn't an integer"
 	rm fileForOutputText.txt
@@ -23,7 +23,7 @@ then
         exit
 fi
 source ../ReverseNumber.sh -23 > fileForOutputText.txt
-if [ "$reversedNumber" != -32 ]
+if [ "$outputMessage" != "Given argument is not an unsigned integer" ]
 then
         echo "failed when the given argument is a negative number"
 	rm fileForOutputText.txt
