2bc35c0b0f64d5b83ac5066b1c56443c995cec3a
ln8-8 <lyut.nersisyan@gmail.com>
2021-11-24 23:26:57 +0400

    Modified minimumDistance.cpp - added the checking of arguments count, used cerr for error, used one style forbrackets, added new print function and splited long lines

--- Graph/minimumDistance.cpp
+++ Graph/minimumDistance.cpp
@@ -3,31 +3,31 @@
 #include <vector>
 #include <limits.h>
 #include <fstream>
+#include <cerrno>
 
-void minimumDistanceOfGraph(int source, int destination, int& best, int& sum, std::vector<int>& used, std::vector<int>& distance, std::vector<std::vector<int>>& minDist, std::vector<std::vector<std::pair<int, int>>>& adjacentNodes)
-{
+void minimumDistanceOfGraph(int source, int destination, 
+		int& best, int& sum, std::vector<int>& used, 
+		std::vector<int>& distance, std::vector<std::vector<int>>& minDist, 
+		std::vector<std::vector<std::pair<int, int>>>& adjacentNodes) {
     used[source] = 1;
     distance.push_back(source);
-    if (sum <= best)
-    {
-        if (source == destination)
-        {
-            if (sum < best)
-            {
+    if (sum <= best) {
+        if (source == destination) {
+            if (sum < best) {
                 minDist.clear();
                 best = sum;
             }
-            if (sum == best)
+            if (sum == best) {
                 minDist.push_back(distance);
+	    }
         }
-        else
-        {
-            for (int i = 0; i < adjacentNodes[source].size(); i++)
-            {
+        else {
+            for (int i = 0; i < adjacentNodes[source].size(); i++) {
                 int node = adjacentNodes[source][i].first;
                 int weight = adjacentNodes[source][i].second;
-                if (used[node])
+                if (used[node]) {
                     continue;
+		}
                 sum += weight;
                 minimumDistanceOfGraph(node, destination, best, sum, used, distance, minDist, adjacentNodes);
                 sum -= weight;
@@ -39,7 +39,9 @@ void minimumDistanceOfGraph(int source, int destination, int& best, int& sum, st
     distance.pop_back();
 }
 
-std::vector<std::vector<int>> minDistance(int& source, int& destination, int& numberOfNodes, int& best, std::vector<std::vector<std::pair<int, int>>>& adjacentNodes) {
+std::vector<std::vector<int>> minDistanceUtility(int& source, int& destination, 
+		int& numberOfNodes, int& best, 
+		std::vector<std::vector<std::pair<int, int>>>& adjacentNodes) {
     int sum = 0;
     std::vector<int> used(numberOfNodes + 1);
     std::vector<int> distance;
@@ -48,21 +50,40 @@ std::vector<std::vector<int>> minDistance(int& source, int& destination, int& nu
     return minDist;
 }
 
-int main(int argc, char** argv)
-{
+void print(std::vector<std::vector<int>> minimumOfDistances, int best) {
+    if (minimumOfDistances.size() == 0) {
+      std::cout << "There is no way from source to destination" << std::endl;
+    }
+    else {
+        std::cout << "The shortest distance is " << best << std::endl;
+        for (int i = 0; i < minimumOfDistances.size(); i++) {
+            for (int j = 0; j < minimumOfDistances[i].size(); j++) {
+                std::cout << minimumOfDistances[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
     int sum = 0;
     int best = INT_MAX;
-    int amountOfNodes = 1;
     int numberOfNodes, numberOfEdges;
     int sourceNode, destinationNode;
+
+    if(argc == 1) {
+	    std::cerr << "You have to input a file" << std::endl;
+	    exit(errno);
+    }
+    else if(argc > 2) {
+	    std::cerr << "Wrong amount of arguments" << std::endl;
+	    exit(errno);
+    }
+
     std::ifstream input;
     input.open(argv[1]);
-    if(argc == 1){
-	    std::cout << "You have to input a file" << std::endl;
-	    exit(1);
-    }
-    if(input.fail())
-    {
+
+    if(input.fail()) {
         std::cerr << "There is no such file" << std::endl;
         exit(1);
     }
@@ -70,35 +91,21 @@ int main(int argc, char** argv)
     input >> numberOfNodes >> numberOfEdges;
     std::vector<std::vector<std::pair<int, int>>> adjacentNodes(numberOfNodes + 1);
 
-    for (int i = 0; i < numberOfEdges; i++)
-    {
+    for (int i = 0; i < numberOfEdges; i++) {
         int firstNode, secondNode, weight;
         input >> firstNode >> secondNode >> weight;
         if (firstNode > numberOfNodes || secondNode > numberOfNodes) {
-            std::cout << "Wrong amount of nodes" << std::endl;
-            amountOfNodes = 0;
-            break;
+            std::cerr << "Wrong amount of nodes" << std::endl;
+	    exit(errno);
         }
         adjacentNodes[firstNode].push_back(std::make_pair(secondNode, weight));
         adjacentNodes[secondNode].push_back(std::make_pair(firstNode, weight));
     }
-    if (amountOfNodes) {
-        input >> sourceNode >> destinationNode;
-        std::vector<std::vector<int>> minimumOfDistances = minDistance(sourceNode, destinationNode, numberOfNodes, best, adjacentNodes);
 
-        if (minimumOfDistances.size() == 0) {
-            std::cout << "There is no way from source to destination" << std::endl;
-        }
-        else {
-            std::cout << "The shortest distance is " << best << std::endl;
-            for (int i = 0; i < minimumOfDistances.size(); i++)
-            {
-                for (int j = 0; j < minimumOfDistances[i].size(); j++)
-                    std::cout << minimumOfDistances[i][j] << " ";
-                std::cout << std::endl;
-            }
-        }
-    }
+    input >> sourceNode >> destinationNode;
+    std::vector<std::vector<int>> minimumOfDistances = minDistanceUtility(sourceNode, destinationNode, numberOfNodes, best, adjacentNodes);
+
+    print(minimumOfDistances, best);
 
     input.close();
     return 0;
